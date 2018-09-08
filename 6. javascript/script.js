a5eeead26a655581945e62b74efbbfe3e0a4646a
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-09-08 11:58:38 +0300

    facebook 2

--- 6. javascript/script.js
+++ 6. javascript/script.js
@@ -398,6 +398,60 @@ do {
 } while (counterTwo > 10);
 */
 
+///////////////////////////////////
+// EXERCISE: BUILDING FACEBOOK 2 //
+///////////////////////////////////
 
+var database = [
+    {
+        username: "andrei",
+        password: "supersecret"
+    },
+    {
+        username: "sally",
+        password: "123"
+    },
+    {
+        username: "ingrid",
+        password: "777"
+    }
+];
+
+var newsFeed = [
+    {
+		username: "Bobby",
+		timeline: "So tired from all that learning!"
+	},
+	{
+		username: "Sally",
+		timeline: "Javascript is sooooo cool!"
+	},
+	{
+		username: "Mitch",
+		timeline: "Javascript is preeetyy cool!"
+	}
+];
+
+function isUserValid(username, password) {
+    for (var i = 0; i < database.length; i++) {
+        if(username === database[i].username && password === database[i].password) {
+            return true;
+        } 
+    }
+    return false;
+}
+
+function signIn(username, password) {
+    if(isUserValid(username, password)) {
+        console.log(newsFeed);
+    } else {
+        alert("Sorry, wrong username and password!");
+    }
+}
+
+var userNamePrompt = prompt("What\'s your user name?");
+var passwordPrompt = prompt("What\'s your password?");
+
+signIn(userNamePrompt, passwordPrompt);
 
 

230cd016e1464413192ef7e2c3afd76c484e8516
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-10-27 16:27:18 +0300

    ASYNC AWAIT

--- 12. ajax/script.js
+++ 12. ajax/script.js
@@ -99,9 +99,107 @@ Promise.all(urls.map(url => fetch(url).then(people => people.json())))
 
 // #6) Change one of your urls above to make it incorrect and fail the promise
 // does your catch block handle it?
-
 */
 
 /////////////////////
 // ES8 ASYNC AWAIT //
 /////////////////////
+
+/*
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(resp => resp.json())
+    .then(console.log);
+
+
+async function fetchUsers() {
+    const resp =await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await resp.json();
+    console.log(data);
+}
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+]
+
+// Promise.all(urls.map(url => {
+//     return fetch(url).then(resp => resp.json())
+// })).then(array => {
+//     console.log('users', array[0]);
+//     console.log('posts', array[1]);
+//     console.log('albums', array[2]);
+// }).catch('oops');
+
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await Promise.all(urls.map(url => 
+            fetch(url).then(resp => resp.json())
+        ))
+        console.log('users', users);
+        console.log('posts', posts);
+        console.log('albums', albums);
+    } catch (err) {
+        console.log('oops');
+    }
+}
+*/
+
+// EXERCISE: ES8 ASYNC AWAIT //
+
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch("https://swapi.co/api/starships/9/")
+    .then(response => response.json())
+    .then(console.log);
+
+async function fetchStarship() {
+    const response = await fetch("https://swapi.co/api/starships/9/");
+    const data = await response.json();
+    console.log(data);
+    }
+
+const urls = [
+    "https://jsonplaceholder.typicode.com/users",
+    "https://jsonplaceholder.typicode.com/posts",
+    "https://jsonplaceholder.typicode.com/albums"
+];
+
+const getData = async function() {
+    const [users, posts, albums] = await Promise.all(
+        urls.map(url => fetch(url).then(resp => resp.json()))
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+};
+
+const getData = async function() {
+    const [users, posts, albums] = await Promise.all(
+        urls.map(async function(url) {
+        const response = await fetch(url);
+        return response.json();
+        })
+    );
+    console.log("users", users);
+    console.log("posta", posts);
+    console.log("albums", albums);
+};
+
+
+// #3) Add a try catch block to the #2 solution in order to catch any errors.
+// Now chnage one of the urls so you console.log your error with 'ooooooops'
+const getData = async function() {
+    try {
+        const [ users, posts, albums ] = await Promise.all(urls.map(async function(url) {
+            const response = await fetch(url);
+            return response.json();
+        }));
+        console.log('users', users);
+        console.log('posta', posts);
+        console.log('albums', albums);
+    } catch (err) {
+        console.log('ooooooops', err);
+    }
+}

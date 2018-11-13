87fa4e7bc5ca336c09fe9dab86eac81e70d074f2
Mantas Masalis <33566613+masalism@users.noreply.github.com>
2018-11-13 20:34:28 +0200

    express.js

--- 13. node.js/server.js
+++ 13. node.js/server.js
@@ -1,15 +1,38 @@
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer((request, response) => {
-    // console.log('headers', request.headers);
-    console.log('method', request.method);
-    console.log('url', request.url);
-    const user = {
-        name: 'John',
-        hoby: 'Skating' 
-    }
-    response.setHeader('Content-Tyoe', 'application/json');
-    response.end(JSON.stringify(user));
+// const server = http.createServer((request, response) => {
+//     // console.log('headers', request.headers);
+//     console.log('method', request.method);
+//     console.log('url', request.url);
+//     const user = {
+//         name: 'John',
+//         hoby: 'Skating' 
+//     }
+//     response.setHeader('Content-Tyoe', 'application/json');
+//     response.end(JSON.stringify(user));
+// });
+
+// server.listen(3000);
+
+const express = require('express');
+
+const app = express();
+
+//GET request
+app.get('/', (req, res) => {
+    res.send("getting root");
 });
 
-server.listen(3000);
+app.get('/profile', (req, res) => {
+    res.send("getting profile");
+});
+
+app.post('/profile', (req, res) => {
+    const user = {
+        name: 'Sally',
+        hobby: 'soccer'
+    }
+    res.send(user);
+});
+
+app.listen(3000);
